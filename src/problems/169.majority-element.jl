dd1833fee18d268798b3481061dfa273f837ad0e
rex <1073853456@qq.com>
2022-03-11 21:45:05 +0800

    add solutions: 169, 297, 946

---
+++ src/problems/169.majority-element.jl
@@ -0,0 +1,60 @@
+# ---
+# title: 169. Majority Element
+# id: problem169
+# author: zhwang
+# date: 2022-03-10
+# difficulty: Easy
+# categories: Array, Divide and Conquer, Bit Manipulation
+# link: <https://leetcode.com/problems/majority-element/description/>
+# hidden: true
+# ---
+# 
+# Given an array of size _n_ , find the majority element. The majority element
+# is the element that appears **more than** `⌊ n/2 ⌋` times.
+# 
+# You may assume that the array is non-empty and the majority element always
+# exist in the array.
+# 
+# **Example 1:**
+# 
+#     
+#     
+#     Input: [3,2,3]
+#     Output: 3
+# 
+# **Example 2:**
+# 
+#     
+#     
+#     Input: [2,2,1,1,1,2,2]
+#     Output: 2
+#     
+# 
+# 
+## @lc code=start
+using LeetCode
+
+function majority_element(nums::Vector{Int})::Int
+    res = vote = 0
+    for num in nums
+        if vote == 0
+            vote += 1
+            res = num
+        else
+            vote += (res == num) ? 1 : -1
+        end
+    end
+    res
+end
+## @lc code=end
+
+## @lc test=start
+@testset "169.majority-element.jl" begin
+    @test majority_element([3,2,3]) == 3
+    @test majority_element([2,2,1,1,1,2,2]) == 2
+end
+## @lc test=end
+
+## @lc info=start
+# link: [solution to 169]()
+## @lc info=end
